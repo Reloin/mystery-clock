4ea558b31d147366e48c98f0c78ed55fd95845db
Reloin <yihengleong@gmail.com>
2021-07-19 10:17:46 +0800

    arranged for development

---
+++ clock/clock.ino
@@ -0,0 +1,17 @@
+/*
+ * 硬设大赛代码，由章鞃玲(组长)，黄鉦皓，梁翊恒共同编写
+ */
+
+//此处输入wifi名和wifi密码
+const char* ssid       = "YOUR_SSID";
+const char* password   = "YOUR_PASS";
+
+void setup()
+{
+  
+}
+
+void loop()
+{
+  
+}
