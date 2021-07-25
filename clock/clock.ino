04e19a5fd65b3779df22e1e056ea2b1aa544b71d
Reloin <yihengleong@gmail.com>
2021-07-25 14:50:12 +0800

    ntp request time & print to serial

--- clock/clock.ino
+++ clock/clock.ino
@@ -5,15 +5,16 @@
 #include <ESP8266WiFi.h>//ESP9266连接WiFi的库
 #include <NTPClient.h>//取得时间的库
 #include <WiFiUdp.h>//进行UDP交流的库
-#include <TimerOne.h>//Time Interupt的库
 
 //此处输入wifi名和wifi密码
 const char* ssid       = "YOUR_SSID";//wifi名
-const char* password   = "YOUR_PASS";//wifi密码
+const char* password   = "WIFI_PWD";//wifi密码
 
 //建立NTP客户端接收时间
 WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP, "asia.pool.ntp.org");
+
+int hour, minute;
 
 void setup()
 {
@@ -29,16 +30,12 @@ void setup()
     timeClient.begin();
     //计算我们时域的时间
     timeClient.setTimeOffset(28800);
+
 }
 
 void loop()
 {
-    //
-}
-
-//检查时间的函数
-void checkTime(){
     timeClient.update();
-    timeClient.getHours();//取得int类的小时
-    timeCleint.getMinustes();//取得int类的分钟
+    Serial.println(timeClient.getFormattedTime());
+    delay(2000);
 }
