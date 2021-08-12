b8f9724073baae7c8f4dffd102ca1631fa355be6
Reloin <yihengleong@gmail.com>
2021-08-12 21:31:41 +0800

    add comments

--- clockhand/clockhand.ino
+++ clockhand/clockhand.ino
@@ -1,18 +1,25 @@
 #include "Stepper.h"
 #include "ESP8266TimerInterrupt.h"
+
+
+//Define pins for stepper motor
 #define Stepper_pin1 D3
 #define Stepper_pin2 D4
 #define Stepper_pin3 D5
 #define Stepper_pin4 D6
+
+//Declare clock stepper motor
 const int stepsPerRevolution = 50;
-Stepper Minhand(stepsPerRevolution,Stepper_pin1,Stepper_pin2,Stepper_pin3,Stepper_pin4);
-ESP8266Timer ITimer;
+Stepper motor(stepsPerRevolution,Stepper_pin1,Stepper_pin2,Stepper_pin3,Stepper_pin4);
+ESP8266Timer moveClock;
+
 void setup() {
-  Minhand.setSpeed(60);
-  ITimer.attachInterruptInterval(18000000, Isr);
+  motor.setSpeed(60);
+  //clock will move for every 18s, as 1 complete revolution is 200 steps
+  moveClock.attachInterruptInterval(18000000, Isr);
 }
 void Isr(){
-    Minhand.step(1);
+    motor.step(1);
 }
 
 void loop() {
