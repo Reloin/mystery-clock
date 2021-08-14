6b4d6ce7c507ea6f825cde3e367a8de0ed0adcd7
Reloin <yihengleong@gmail.com>
2021-08-15 07:21:20 +0800

    seperate into functions

--- attiny/attiny.ino
+++ attiny/attiny.ino
@@ -1,128 +1,143 @@
 #include "pitches.h"
-void song(int n){
-  switch(n){
-    case 1:
-            int melody[] = {//周杰伦晴天 for 晴天
-                            NOTE_B5,NOTE_A5,NOTE_C6,NOTE_B5,NOTE_G5,NOTE_D6,NOTE_FS6,NOTE_G6,NOTE_FS6,NOTE_B5,NOTE_G5,
-                            NOTE_G5,NOTE_E6,NOTE_E6,0,NOTE_E6,NOTE_D6,NOTE_D6,NOTE_D6,NOTE_C6,NOTE_B5,NOTE_A5,NOTE_B5,NOTE_C6,NOTE_B5
-                          };
 
-            int noteDurations[] = {
+const int red=0, green=1, blue=2, pin_buzzer = 4, input = 3;
 
-              2,2,2,1,2,2,2,2,2,2,1,
-              2,2,2,2,2,2,1,2,2,2,2,2,2,1
-            };
-            for (int thisNote = 0; thisNote < 25; thisNote++) {
+//1晴
+void sunny(){
+  int melody[] = {//周杰伦晴天 for 晴天
+    NOTE_B5,NOTE_A5,NOTE_C6,NOTE_B5,NOTE_G5,NOTE_D6,NOTE_FS6,NOTE_G6,NOTE_FS6,NOTE_B5,NOTE_G5,
+    NOTE_G5,NOTE_E6,NOTE_E6,0,NOTE_E6,NOTE_D6,NOTE_D6,NOTE_D6,NOTE_C6,NOTE_B5,NOTE_A5,NOTE_B5,NOTE_C6,NOTE_B5
+  };
 
-            int noteDuration = 500 / noteDurations[thisNote];
+  int noteDurations[] = {
+    2,2,2,1,2,2,2,2,2,2,1,
+    2,2,2,2,2,2,1,2,2,2,2,2,2,1
+  };
+  for (int thisNote = 0; thisNote < 25; thisNote++) {
 
-            tone(pin_buzzer, melody[thisNote], noteDuration);
+    int noteDuration = 500 / noteDurations[thisNote];
 
-            int pauseBetweenNotes = noteDuration * 1.30;
+    tone(pin_buzzer, melody[thisNote], noteDuration);
 
-            delay(pauseBetweenNotes);
+    int pauseBetweenNotes = noteDuration * 1.30;
 
-            noTone(pin_buzzer);
+    delay(pauseBetweenNotes);
 
-            }
-            break;
-    case 2:
-            int melody[] = {//别找我麻烦蔡健雅
+    noTone(pin_buzzer);
 
-              NOTE_G5,NOTE_F5,NOTE_G5,NOTE_F5,NOTE_G5,NOTE_F5,NOTE_G5,
-              0,NOTE_B6,NOTE_G5,NOTE_F5,NOTE_G5,NOTE_F5,NOTE_G5,NOTE_F5,NOTE_E5,NOTE_E5,NOTE_E5,NOTE_F5
-
-            };
-
-            int noteDurations[] = {
-
-            2,2,2,2,2,4,4,2,4,2,2,2,2,4,2,4,2,2,1
-            };
-            for (int thisNote = 0; thisNote <19; thisNote++) {
-
-            int noteDuration = 500 / noteDurations[thisNote];
-
-            tone(pin_buzzer, melody[thisNote], noteDuration);
-              
-            int pauseBetweenNotes = noteDuration * 1.30;
-
-            delay(pauseBetweenNotes);
-
-            noTone(pin_buzzer);
-
-          }
-          break;
-    case 3:
-           int melody[] = {//冰雨刘德华
-
-              NOTE_G5,NOTE_G5,NOTE_C6,NOTE_A5,NOTE_G5,NOTE_A5,NOTE_G5,NOTE_G5,NOTE_G5,NOTE_G5,NOTE_E5,NOTE_G5,
-              NOTE_G5,NOTE_G5,NOTE_C6,NOTE_A5,NOTE_G5,NOTE_A5,NOTE_G5,NOTE_G5,NOTE_G5,NOTE_G5,NOTE_D5,NOTE_E5
-
-            };
-
-            double noteDurations[] = {
-
-            4,4,4,4,4,4,2,4,4,2,4,(4/3),
-              4,4,4,4,4,4,2,4,4,2,4,1
-            }; 
-            for (int thisNote = 0; thisNote <24; thisNote++) {
-
-            double noteDuration = 800/ noteDurations[thisNote];
-
-            tone(pin_buzzer, melody[thisNote], noteDuration);
-
-            int pauseBetweenNotes = noteDuration * 1.30;
-
-            delay(pauseBetweenNotes);
-
-            noTone(pin_buzzer);
-
-            }
-            break;
-    case 5://认真的雪薛之谦
-            int melody[] = {
-
-              NOTE_G5,NOTE_G5,NOTE_G5,NOTE_A5,NOTE_C6,NOTE_E6,NOTE_G5,NOTE_G5,NOTE_G5,NOTE_A5,NOTE_C6,NOTE_D6,
-              NOTE_D6,0,NOTE_D6,NOTE_E6,NOTE_D6,NOTE_D6,NOTE_C6,NOTE_C6,NOTE_C6,NOTE_C6,NOTE_C6,NOTE_G6,NOTE_E6
-
-            };
-
-            double noteDurations[] = {
-
-            4,4,4,4,4,4/3,4,4,4,4,4,4,
-               1,4,4,4,4,4,4,4,4,4,4,4,2,1
-            };
-            for (int thisNote = 0; thisNote <25; thisNote++) {
-
-            double noteDuration = 800/ noteDurations[thisNote];
-
-            tone(pin_buzzer, melody[thisNote], noteDuration);
-
-            int pauseBetweenNotes = noteDuration * 1.30;
-
-            delay(pauseBetweenNotes);
-
-            noTone(pin_buzzer);
-
-            }
-            break;
-
-      
-    default:break;
   }
-  
 }
-// notes in the melody:
-int melody[] = {
-  NOTE_C4, NOTE_G3, NOTE_G3, NOTE_A3, NOTE_G3, 0, NOTE_B3, NOTE_C4
-};
 
-// note durations: 4 = quarter note, 8 = eighth note, etc.:
-int noteDurations[] = {
-  4, 8, 8, 4, 4, 4, 4, 4
-};
+//2阴
+void cloudy(){
+  int melody[] = {//别找我麻烦蔡健雅
 
-const int red=0, green=1, blue=2, speaker = 4, input = 3;
+    NOTE_G5,NOTE_F5,NOTE_G5,NOTE_F5,NOTE_G5,NOTE_F5,NOTE_G5,
+    0,NOTE_B6,NOTE_G5,NOTE_F5,NOTE_G5,NOTE_F5,NOTE_G5,NOTE_F5,NOTE_E5,NOTE_E5,NOTE_E5,NOTE_F5
+
+  };
+
+  int noteDurations[] = {
+
+  2,2,2,2,2,4,4,2,4,2,2,2,2,4,2,4,2,2,1
+  };
+  for (int thisNote = 0; thisNote <19; thisNote++) {
+
+    int noteDuration = 500 / noteDurations[thisNote];
+
+    tone(pin_buzzer, melody[thisNote], noteDuration);
+      
+    int pauseBetweenNotes = noteDuration * 1.30;
+
+    delay(pauseBetweenNotes);
+
+    noTone(pin_buzzer);
+
+  }
+}
+
+//3雨
+void rain(){
+  int melody[] = {//冰雨刘德华
+    NOTE_G5,NOTE_G5,NOTE_C6,NOTE_A5,NOTE_G5,NOTE_A5,NOTE_G5,NOTE_G5,NOTE_G5,NOTE_G5,NOTE_E5,NOTE_G5,
+    NOTE_G5,NOTE_G5,NOTE_C6,NOTE_A5,NOTE_G5,NOTE_A5,NOTE_G5,NOTE_G5,NOTE_G5,NOTE_G5,NOTE_D5,NOTE_E5
+  };
+
+  double noteDurations[] = {
+  4,4,4,4,4,4,2,4,4,2,4,(4/3),
+    4,4,4,4,4,4,2,4,4,2,4,1
+  }; 
+  for (int thisNote = 0; thisNote <24; thisNote++) {
+
+    double noteDuration = 800/ noteDurations[thisNote];
+
+    tone(pin_buzzer, melody[thisNote], noteDuration);
+
+    int pauseBetweenNotes = noteDuration * 1.30;
+
+    delay(pauseBetweenNotes);
+
+    noTone(pin_buzzer);
+
+  }
+}
+
+//5雪
+void snow(){
+  int melody[] = {
+    NOTE_G5,NOTE_G5,NOTE_G5,NOTE_A5,NOTE_C6,NOTE_E6,NOTE_G5,NOTE_G5,NOTE_G5,NOTE_A5,NOTE_C6,NOTE_D6,
+    NOTE_D6,0,NOTE_D6,NOTE_E6,NOTE_D6,NOTE_D6,NOTE_C6,NOTE_C6,NOTE_C6,NOTE_C6,NOTE_C6,NOTE_G6,NOTE_E6
+  };
+
+  double noteDurations[] = {
+
+  4,4,4,4,4,4/3,4,4,4,4,4,4,
+  1,4,4,4,4,4,4,4,4,4,4,4,2,1
+  };
+
+  for (int thisNote = 0; thisNote <25; thisNote++) {
+
+    double noteDuration = 800/ noteDurations[thisNote];
+
+    tone(pin_buzzer, melody[thisNote], noteDuration);
+
+    int pauseBetweenNotes = noteDuration * 1.30;
+
+    delay(pauseBetweenNotes);
+
+    noTone(pin_buzzer);
+
+  }
+}
+
+//melody for alarm
+void alarm(){
+  int melody[] = {
+    NOTE_G3,NOTE_G3,NOTE_G3,NOTE_DS3,NOTE_AS3,
+    NOTE_D3,NOTE_D3,NOTE_D3,NOTE_DS3,NOTE_AS3,NOTE_FS3,NOTE_DS3,NOTE_G3
+  };
+
+  double noteDurations[] = {
+    4,4,4,8/3,8,4,8/3,8,
+    4,4,4,8/3,8,4,8/3,8,2
+  };
+
+  for (int thisNote = 0; thisNote <25; thisNote++) {
+
+    double noteDuration = 800/ noteDurations[thisNote];
+
+    tone(pin_buzzer, melody[thisNote], noteDuration);
+
+    int pauseBetweenNotes = noteDuration * 1.30;
+
+    delay(pauseBetweenNotes);
+
+    noTone(pin_buzzer);
+
+  }
+}
+
+
 void setup() {
   // put your setup code here, to run once:
   pinMode(input, INPUT);
@@ -140,7 +155,7 @@ void rgb(int r,int g,int b){
 
 void weatherswitch(int weather){
   switch(weather){
-    case 1://晴
+    case 1://
           rgb(102, 255, 255);
           delay(4500);
           break;
@@ -184,23 +199,6 @@ void weatherswitch(int weather){
   }
 }
 
-void song(){
-  for (int thisNote = 0; thisNote < 8; thisNote++) {
-
-    // to calculate the note duration, take one second divided by the note type.
-    //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
-    int noteDuration = 1000 / noteDurations[thisNote];
-    tone(speaker, melody[thisNote], noteDuration);
-
-    // to distinguish the notes, set a minimum time between them.
-    // the note's duration + 30% seems to work well:
-    int pauseBetweenNotes = noteDuration * 1.30;
-    delay(pauseBetweenNotes);
-    // stop the tone playing:
-    noTone(speaker);
-  }
- }
-
 void loop(){
     //read pwm high time
     float val = pulseIn(input, HIGH);
@@ -208,14 +206,18 @@ void loop(){
     switch (int(val * 10.23 / 2000))
     {
     case 1:
+      sunny();
       break;
     case 2:
+      cloudy();
       break;
     case 3:
+      rain();
       break;
     case 4:
       break;
     case 5:
+      snow();
       break;
     case 6:
       break;
@@ -226,8 +228,13 @@ void loop(){
     case 9:
       break;
     case 10:
+      alarm();
       break;
     default:
+      noTone(pin_buzzer);
+      digitalWrite(red, HIGH);
+      digitalWrite(green, HIGH);
+      digitalWrite(blue, HIGH);
       break;
     }
     
